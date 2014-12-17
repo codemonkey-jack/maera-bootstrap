e64822fe6ff9cc9bebe305794f54368984a5a2b3
fovoc <kalliris.d@gmail.com>
2014-12-17 20:22:14 +0200

    remove most of social links - fixes #37

--- classes/class-Maera_BS_Social.php
+++ classes/class-Maera_BS_Social.php
@@ -9,27 +9,9 @@ class Maera_BS_Social {
 
 	public static function social_networks() {
 		return array(
-			'blogger'     => __( 'Blogger', 'maera_bootstrap' ),
-			'deviantart'  => __( 'DeviantART', 'maera_bootstrap' ),
-			'digg'        => __( 'Digg', 'maera_bootstrap' ),
-			'dribbble'    => __( 'Dribbble', 'maera_bootstrap' ),
 			'facebook'    => __( 'Facebook', 'maera_bootstrap' ),
-			'flickr'      => __( 'Flickr', 'maera_bootstrap' ),
-			'github'      => __( 'Github', 'maera_bootstrap' ),
-			'googleplus'  => __( 'Google+', 'maera_bootstrap' ),
-			'instagram'   => __( 'Instagram', 'maera_bootstrap' ),
-			'linkedin'    => __( 'LinkedIn', 'maera_bootstrap' ),
-			'myspace'     => __( 'MySpace', 'maera_bootstrap' ),
-			'pinterest'   => __( 'Pinterest', 'maera_bootstrap' ),
-			'reddit'      => __( 'Reddit', 'maera_bootstrap' ),
-			'rss'         => __( 'RSS', 'maera_bootstrap' ),
-			'skype'       => __( 'Skype', 'maera_bootstrap' ),
-			'soundcloud'  => __( 'SoundCloud', 'maera_bootstrap' ),
-			'tumblr'      => __( 'Tumblr', 'maera_bootstrap' ),
 			'twitter'     => __( 'Twitter', 'maera_bootstrap' ),
-			'vimeo'       => __( 'Vimeo', 'maera_bootstrap' ),
-			'vkontakte'   => __( 'Vkontakte', 'maera_bootstrap' ),
-			'youtube'     => __( 'YouTube', 'maera_bootstrap' ),
+			'googleplus'  => __( 'Google+', 'maera_bootstrap' ),
 		);
 	}
 
@@ -46,7 +28,7 @@ class Maera_BS_Social {
 			$link = get_theme_mod( $social_link . '_link', '' );
 
 			if ( '' != esc_url( $link ) ) {
-				$content .= '<a role="link" aria-labelledby="' . $label . '" href="' . $link . '" target="_blank" title="' . $label . '"><i class="el-icon-' . $social_link . '"></i>';
+				$content .= '<a role="link" aria-labelledby="' . $label . '" href="' . $link . '" target="_blank" title="' . $label . '"><i class="dashicons dashicons-' . $social_link . '"></i>';
 				$content .= 'dropdown' == get_theme_mod( 'navbar_social', 'off' ) ? '&nbsp;' . $label : '';
 				$content .= '</a>';
 				$content .= $separator;
@@ -82,7 +64,7 @@ class Maera_BS_Social {
 
 		} elseif ( 'dropdown' == $social_mode ) {
 
-			$before = ( $navbar_position == 'right-slide' || $navbar_position == 'left-slide' ) ? '<ul class="nav navbar-nav navbar-dropdown-socials"><li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false"><i class="el-icon-network"></i>&nbsp;<b class="caret"></b></a><ul class="dropdown-menu" role="menu"><li>' : '<ul class="nav navbar-nav navbar-right navbar-dropdown-socials"><li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false"><i class="el-icon-network"></i>&nbsp;<b class="caret"></b></a><ul class="dropdown-menu" role="menu"><li>';
+			$before = ( $navbar_position == 'right-slide' || $navbar_position == 'left-slide' ) ? '<ul class="nav navbar-nav navbar-dropdown-socials"><li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false"><i class="dashicons dashicons-share"></i>&nbsp;<b class="caret"></b></a><ul class="dropdown-menu" role="menu"><li>' : '<ul class="nav navbar-nav navbar-right navbar-dropdown-socials"><li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false"><i class="dashicons dashicons-share"></i>&nbsp;<b class="caret"></b></a><ul class="dropdown-menu" role="menu"><li>';
 			$after     = '</li></ul></li></ul>';
 			$separator = '</li><li>';
 
